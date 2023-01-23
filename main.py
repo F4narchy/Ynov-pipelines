aceca877ceecbceb265785cce9333fac889aa00f
F4narchy <92426319+F4narchy@users.noreply.github.com>
2023-01-23 12:27:20 +0100

    Changements fichiers intervenant

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from bottle import route, run
-
-from sum import sum
-
-
-@route('/')
-def index():
-    return "Hello World"
+from bottle import route, run, template
+from misc import add
 
 
-@route('/add/<a>/<b>')
-def add(a, b):
-    return {'result': sum(a, b)}
+@route("/hello/<name>")
+def index(name):
+    return template("<b>Hello {{name}}</b>!", name=name)
 
 
-run(host='localhost', port=8080)
+@route("/")
+def homepage():
+    return "<b>Hello you</b>!"
+
+
+@route("/add/<a>/<b>")
+@route("/add/<a>/<b>/")
+def route_add(a, b):
+    return {"result": add(a, b)}
+
+
+run(host="localhost", port=8080, reloader=True)
--- sum.py
+++
@@ -1,7 +0,0 @@
-def sum(a, b):
-    """Compute and return the sum of two numbers.
-    Usage examples:
-    >>> sum(4, 2)
-    6
-    """
-    return int(a) + int(b)
